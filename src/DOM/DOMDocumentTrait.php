cb209035b41277a6b52838dc0acaa16945ba287e
Salvatore Santagati <me@salsan.dev>
2024-04-21 14:25:29 +0200

    feat: added function return value from DOM node

--- src/DOM/DOMDocumentTrait.php
+++ src/DOM/DOMDocumentTrait.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Salsan\Utils\DOM;
 
 use DOMDocument;
+use DOMXPath;
 
 trait DOMDocumentTrait
 {
@@ -17,5 +18,13 @@ trait DOMDocumentTrait
         $dom->loadHTML(($charset) ? '<!DOCTYPE html><meta charset="' . $charset . '">' . $html  : $html); // workaround for issue #2 https://stackoverflow.com/a/8218649/1501286
 
         return $dom;
+    }
+
+    public function getNodeValue(DOMXPath $xpath, string $node): string
+    {
+        $getNode = $xpath->query($node);
+        $getValue =  $getNode->length > 0 ? $this->trimmer($getNode->item(0)->nodeValue) : '';
+
+        return $getValue;
     }
 }
